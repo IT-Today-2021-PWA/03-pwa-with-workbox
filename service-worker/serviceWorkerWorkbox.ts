51def6255b6cae42864b8d6e22e14992492ec8a8
Rahmanda Wibowo <rahmandawibowo@gmail.com>
2021-09-08 15:40:55 +0200

    feat: cache api and images

--- service-worker/serviceWorkerWorkbox.ts
+++ service-worker/serviceWorkerWorkbox.ts
@@ -1,6 +1,9 @@
 // / <reference lib="webworker" />
 import { createHandlerBoundToURL, precacheAndRoute } from 'workbox-precaching';
 import { registerRoute, NavigationRoute } from 'workbox-routing';
+import { CacheFirst, StaleWhileRevalidate } from 'workbox-strategies';
+import { CacheableResponsePlugin } from 'workbox-cacheable-response';
+import { ExpirationPlugin } from 'workbox-expiration';
 import { skipWaiting, clientsClaim } from 'workbox-core';
 
 declare const self: Window & ServiceWorkerGlobalScope;
@@ -16,3 +19,35 @@ const navigationRoute = new NavigationRoute(handler, {
   denylist: [/^\/_/, /\/[^/?]+\.[^/]+$/],
 });
 registerRoute(navigationRoute);
+
+registerRoute(
+  (event) => event.request.url.startsWith('https://api.jikan.moe/'),
+  new StaleWhileRevalidate({
+    cacheName: 'api.jikan.moe',
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [200]
+      }),
+      new ExpirationPlugin({
+        maxEntries: 100,
+        maxAgeSeconds: 24 * 60 * 60
+      })
+    ]
+  })
+);
+
+registerRoute(
+  (event) => event.request.url.startsWith('https://cdn.myanimelist.net/images/anime/'),
+  new CacheFirst({
+    cacheName: 'myanimelist-images',
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0, 200]
+      }),
+      new ExpirationPlugin({
+        maxEntries: 100,
+        maxAgeSeconds: 14 * 24 * 60 * 60,
+      })
+    ]
+  })
+);
